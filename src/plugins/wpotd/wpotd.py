c011c9e151e885188de66d38c1a1aca07c9412d6
phattmatt <19799332+phattmatt@users.noreply.github.com>
2025-06-14 11:28:34 +0100

    Added user-agent as per https://foundation.wikimedia.org/wiki/Policy:Wikimedia_Foundation_User-Agent_Policy. Basic functionality works.

--- src/plugins/wpotd/wpotd.py
+++ src/plugins/wpotd/wpotd.py
@@ -10,9 +10,11 @@ from io import BytesIO
 import requests
 import logging
 from random import randint
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 
 logger = logging.getLogger(__name__)
+SESSION = requests.Session()
+headers = {'User-Agent': 'InkyPi/0.0 (https://github.com/fatihak/InkyPi/)'}
 
 class Wpotd(BasePlugin):
     def generate_settings_template(self):
@@ -23,28 +25,37 @@ class Wpotd(BasePlugin):
     def generate_image(self, settings, device_config):
         logger.info(f"WPOTD plugin settings: {settings}")
 
-        params = {}
+        #params = {}
 
         if settings.get("randomizeWpotd") == "true":
             start = datetime(2015, 1, 1)
             end = datetime.today()
             delta_days = (end - start).days
             random_date = start + timedelta(days=randint(0, delta_days))
-            params["date"] = random_date.strftime("%Y-%m-%d")
+            datetofetch = random_date.date()
+            #params["date"] = random_date.strftime("%Y-%m-%d")
         elif settings.get("customDate"):
-            params["date"] = settings["customDate"]
+            #params["date"] = settings["customDate"]
+            datetofetch = datetime.strptime(settings["customDate"], "%Y-%m-%d").date()
 
-        data = self.fetch_potd(params["date"])
+        logger.info(f"WPOTD plugin datetofetch: {datetofetch}")
+        data = self.fetch_potd(datetofetch)
+        picurl = data["image_src"]
+        logger.info(f"WPOTD plugin Picture URL: {picurl}")
 
         try:
-            img_data = requests.get(data["image_src"])
-            image = Image.open(BytesIO(img_data.content))
+            img_data = SESSION.get(picurl, headers=headers)
+            if img_data.status_code != 200:
+                logger.info(f"WPOTD plugin get images status code: {img_data.status_code}")
+                raise RuntimeError("Failed to download image.")
+            else:
+                image = Image.open(BytesIO(img_data.content))
         except Exception as e:
-            logger.error(f"Failed to load APOD image: {str(e)}")
-            raise RuntimeError("Failed to load APOD image.")
+            logger.error(f"Failed to load WPOTD image: {str(e)}")
+            raise RuntimeError("Failed to load WPOTD image.")
 
         return image
-    
+
     def fetch_potd(self, cur_date):
         """
         Returns image data related to the current POTD
@@ -61,7 +72,7 @@ class Wpotd(BasePlugin):
             "titles": title
         }
 
-        response = requests.get(url="https://en.wikipedia.org/w/api.php", params=params)
+        response = SESSION.get(url="https://en.wikipedia.org/w/api.php", params=params)
         data = response.json()
 
         filename = data["query"]["pages"][0]["images"][0]["title"]
@@ -91,7 +102,7 @@ class Wpotd(BasePlugin):
            "titles": filename
         }
 
-        response = requests.get(url="https://en.wikipedia.org/w/api.php", params=params)
+        response = SESSION.get(url="https://en.wikipedia.org/w/api.php", params=params)
         data = response.json()
         page = next(iter(data["query"]["pages"].values()))
         image_info = page["imageinfo"][0]
